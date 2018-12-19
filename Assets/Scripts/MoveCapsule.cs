1d8f9a5c48fa7c0f0a3468658b4de21b4265631a
Gonzy27 <gonzaloespinoza.alarcon@gmail.com>
2018-12-19 01:48:03 -0300

    Arreglo de movimiento de jugador, cámara sigue a jugador y game over

--- Assets/Scripts/MoveCapsule.cs
+++ Assets/Scripts/MoveCapsule.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MoveCapsule : MonoBehaviour
@@ -8,7 +9,7 @@ public class MoveCapsule : MonoBehaviour
 
     private float moveHorizontal;
     private float moveVertical;
-    private bool gets_shoot = false;
+    private bool gets_shoot;
     private Vector3 hit_direction;
     private Vector3 initial_position;
 
@@ -19,22 +20,28 @@ public class MoveCapsule : MonoBehaviour
     private float flash_counter;
     public MeshRenderer mesh_renderer;
 
-    private int health;
-
     public Rigidbody rb;
 
+    //velocidad, salto, gravedad y fuerza knockback
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public float knockback_force;
 
+    //Texto
     public Text health_points;
+    public Text game_over;
 
+    //¿muerto?
+    private bool player_dead;
+    private bool restart;
+
+    public int health;
+
+    //movimiento
     public Vector3 movement = Vector3.zero;
-
     public CharacterController characterController;
 
-    public float cronometro;
 
     public LocalNavMeshBuilder NAV;
 
@@ -43,6 +50,9 @@ public class MoveCapsule : MonoBehaviour
     {
         health = 100;
         health_points.text = "Vida: " + health;
+        gets_shoot = false;
+        game_over.text = "";
+        player_dead = false;
         // let the gameObject fall down
         //gameObject.transform.position = new Vector3(0, 5, 0);
     }
@@ -50,71 +60,83 @@ public class MoveCapsule : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
+        if (!player_dead)
         {
-            moveHorizontal = Input.GetAxis("Horizontal");
-            moveVertical = Input.GetAxis("Vertical");
-            movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-            //Debug.Log("Rot: " + movement.ToString());
-            if (movement != Vector3.zero)
-                transform.rotation = Quaternion.LookRotation(movement);
-
-
-            movement = movement * speed;
-
-
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (characterController.isGrounded)
             {
-                movement.y = jumpSpeed;
+                moveHorizontal = Input.GetAxis("Horizontal");
+                moveVertical = Input.GetAxis("Vertical");
+                movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+                //Debug.Log("Rot: " + movement.ToString());
+                if (movement != Vector3.zero)
+                    transform.rotation = Quaternion.LookRotation(movement);
+
+
+                movement = movement * speed;
+
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    movement.y = jumpSpeed;
+                }
             }
-        }
-        else  // Here I independently allow for both X and Z movement. 
-        {
-
-            movement.x = Input.GetAxis("Horizontal") * speed;
-            movement.z = Input.GetAxis("Vertical") * speed;
-            transform.rotation = Quaternion.LookRotation(new Vector3(movement.x, 0.0f, movement.z));
-            //movement = new Vector3(moveHorizontal, movement.y, moveVertical);
-
-        }
-
-
-
-        //aplica gravedad
-        movement.y = movement.y - (gravity * Time.deltaTime);
-
-        if (gets_shoot)
-        {
-
-            transform.position += hit_direction * knockback_force * Time.deltaTime;
-
-            if (Vector3.Distance(transform.position, initial_position) > 4.0f)
+            else  // Here I independently allow for both X and Z movement. 
             {
-                gets_shoot = false;
+                movement.x = Input.GetAxis("Horizontal") * speed;
+                movement.z = Input.GetAxis("Vertical") * speed;
+                transform.rotation = Quaternion.LookRotation(new Vector3(movement.x, 0.0f, movement.z));
+                //movement = new Vector3(moveHorizontal, movement.y, moveVertical);
+
             }
 
 
+
+            //aplica gravedad
+            movement.y = movement.y - (gravity * Time.deltaTime);
+
+            if (gets_shoot)
+            {
+                transform.position += hit_direction * knockback_force * Time.deltaTime;
+
+                if (Vector3.Distance(transform.position, initial_position) > 4.0f)
+                {
+                    gets_shoot = false;
+                }
+
+
+            }
+            else
+                characterController.Move(movement * Time.deltaTime);
+
+            if (invisibility_counter > 0)
+            {
+                invisibility_counter -= Time.deltaTime;
+                flash_counter -= Time.deltaTime;
+                if (flash_counter <= 0)
+                {
+                    flash_counter = flash_lenght;
+                    mesh_renderer.enabled = !mesh_renderer.enabled;
+                }
+                if (invisibility_counter <= 0)
+                {
+                    mesh_renderer.enabled = true;
+                }
+            }
         }
         else
-            characterController.Move(movement * Time.deltaTime);
-
-        if (invisibility_counter > 0)
         {
-            Debug.Log("entra?");
-            invisibility_counter -= Time.deltaTime;
-            flash_counter -= Time.deltaTime;
-            if (flash_counter <= 0)
+            movement.y = movement.y - (gravity * Time.deltaTime);
+            movement.x = 0.0f;
+            movement.z = 0.0f;
+            characterController.height = 1.0f;
+            characterController.Move(movement * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(new Vector3(0.0f, 90.0f, 0.0f));
+            if (Input.GetKeyDown(KeyCode.R))
             {
-                flash_counter = flash_lenght;
-                mesh_renderer.enabled = !mesh_renderer.enabled;
-            }
-            if (invisibility_counter <= 0)
-            {
-                mesh_renderer.enabled = true;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
-
     }
 
     //void OnTriggerEnter(Collider other
@@ -124,11 +146,12 @@ public class MoveCapsule : MonoBehaviour
 
         //if (cronometro < Time.timeSinceLevelLoad)
         if (invisibility_counter <= 0)
+        {
 
             if (collision.gameObject.tag == "enemy")
             {
 
-                //Debug.Log("Siente el choque");
+                Debug.Log("Siente el choque");
                 //cronometro = Time.timeSinceLevelLoad + 3.0f; //crónometro
 
                 initial_position = transform.position;
@@ -147,9 +170,12 @@ public class MoveCapsule : MonoBehaviour
                 }
                 else
                 {
+                    Debug.Log("RIP");
                     health_points.text = "Vida: 0";
-                    Destroy(gameObject);
+                    game_over.text = "BOI YOU DEAD";
+                    player_dead = true;
                 }
             }
+        }
     }
 }
---
+++ Assets/Scripts/follow_player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_player : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 camera_movement;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera_movement = player.transform.position - transform.position;
+
+        // Debug.Log(player.transform.position.y);
+
+        //Debug.Log(transform.position.y);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //transform.LookAt(player.transform);
+        transform.position = player.transform.position - camera_movement;
+
+    }
+}
